94f583b77a352d264d1416e474362380e4d6401f
MuhRezaAldiIrawan <titaniaelvs@gmail.com>
2024-05-06 17:36:07 +0800

    wip

--- master-data/index.js
+++ master-data/index.js
@@ -36,7 +36,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/portalaccount', portalAccountRoutes);
 app.use('/api', redemLogsRoutes);
 
-// Start the server
+// Start the server Node
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
